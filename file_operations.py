4a5639b39faa2a8bc7f6f03e11e47c0cedd63a4d
urraswow <urraswow@student.21-school.ru>
2024-10-25 13:48:14 +0300

    Work with stash

--- file_operations.py
+++ file_operations.py
@@ -1,11 +1,14 @@
 def write_file(filename, content):
+    print("This function of write")
     with open(filename, 'w') as file:
         file.write(content)
     print(f"Content written to {filename}")
     print("Success")
+    print("Good luck")
 
 def read_file(filename):
     print('This function of read')
+    print("Hello world")
     try:
         with open(filename, 'r') as file:
             return file.read()
